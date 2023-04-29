69186031e0df6bb49900caab17d69b1b8f6f2e2e
AVIRAL SINGH <singhaviral0704@gmail.com>
2023-04-29 15:17:11 +0530

    refractor the code

--- src/com/avi/Java8_Features/StreamExampleObject.java
+++ src/com/avi/Java8_Features/StreamExampleObject.java
@@ -14,12 +14,23 @@ public class StreamExampleObject {
         empList.add(new Employee(1,"Anuvi",23,"F","Card",2019,80000.00,"SeniorAss","Noida"));
         empList.add(new Employee(1,"Amit",34,"M","FRMS",2020,120000.00,"Specialist","Pune"));
         empList.add(new Employee(1,"Heena",27,"F","Card",2017,90000.00,"Professional","Pune"));
-        empList.add(new Employee(1,"zubin",28,"M","FRMS",2011,95000.00,"Professional","Noida"));
-        //empList.add(new Employee(1,"Aviral",28,"M","FRMS",2021,100000.00,"Professional","Pune"));
-       // empList.add(new Employee(1,"Aviral",28,"M","FRMS",2021,100000.00,"Professional","Pune"));
+        empList.add(new Employee(1,"Anuvi",28,"M","FRMS",2011,95000.00,"Professional","Noida"));
 
-    //Fetch the employees names whose age are greater than 25 and designations are professional
-       // List<Employee> tempList= empList.stream().filter(e -> e.getAge()>25 && "Professional".equalsIgnoreCase(e.getDesignation())).collect(Collectors.toList());
+        //Important questions for Interviews
+
+        //Q1Fetch the employees names whose age are greater than 25 and designations are professional
+        List<Employee> empgreater25desprofessional=empList.stream().filter(e ->(e.getAge()>25 && "Professional".equalsIgnoreCase(e.getDesignation()))).collect(Collectors.toList());
+
+        //Q2Fetch the employees names whose location is pune
+         empList.stream().filter(e ->e.getLoc().equalsIgnoreCase("Pune")).map(Employee::getName).forEach(System.out::println);
+
+        //Q3Fetch the unique employees names in the emp list
+         Set<String> uniquenames=new HashSet<>();
+         empList.stream().filter(e->!uniquenames.add(e.getName())).map(Employee::getName).forEach(System.out::println);
+
+        //Q4Fetch names of all the departments
+        empList.stream().map(e->e.getDepartment()).distinct().forEach(System.out::println);
+
        // List<Employee> tempList= empList.stream().filter(e -> "Pune".equalsIgnoreCase(e.getLoc())).collect(Collectors.toList());
        // Stream<Employee> tempList= empList.stream().filter(e -> "Pune".equalsIgnoreCase(e.getLoc())).sorted();
        // tempList.stream().forEach(e -> System.out.println(e.getName()));
@@ -37,16 +48,23 @@ public class StreamExampleObject {
         List<String> listOfString = Arrays.asList("Java", "C", "C++", "Go", "JavaScript", "Python", "Scala");
 
         List<String> listOfStringStartsWithJ = listOfString.stream() .filter(s -> s.startsWith("J")) .collect(Collectors.toList());
-
-        System.out.println("list of String starts with letter J: " + listOfStringStartsWithJ);
+      //  System.out.println("list of String starts with letter J: " + listOfStringStartsWithJ);
         Optional<Employee> highestPaidEmployeeWrapper=
                 empList.stream().collect(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)));
 
 
         Employee highestPaidEmployee = highestPaidEmployeeWrapper.get();
-        System.out.println("Details Of Highest Paid Employee : "+highestPaidEmployee);
+      //  System.out.println("Details Of Highest Paid Employee : "+highestPaidEmployee);
      Optional<Double> salary=empList.stream().map(e->e.getSalary()).sorted(Comparator.reverseOrder()).skip(2).findFirst();
       // List<String> names = (List<String>) empList.stream().collect(Collectors.toList(Comparator.comparing(Employee::getName)));
+
+
+
+        //System.out.println(noOfMaleAndFemaleEmployees);
+        List<String> listOfStringBag = Arrays.asList("Bags", "Ba", "Banner", "Baggage");
+
+        List<String> listOfStringStartsWithBa = listOfStringBag.stream() .filter(s ->s.startsWith("Bag")) .collect(Collectors.toList());
+       // System.out.println("list of String starts with  Ba: " + listOfStringStartsWithBa);
 
 
     }
